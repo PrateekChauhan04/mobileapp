722fe3b01c9523dfef29fd9eed74d16ead5a9f92
PrateekChauhan04 <51450072+PrateekChauhan04@users.noreply.github.com>
2020-01-02 23:40:45 +0530

    mobapp

---
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/service/UserService.java
@@ -0,0 +1,11 @@
+package com.appdeveloperblog.app.ws.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.appdeveloperblog.app.ws.shared.dto.UserDto;
+
+public interface UserService extends UserDetailsService {
+UserDto	createUser(UserDto user);
+UserDto getUser(String email);
+UserDto getUserByUserId(String userId);
+}
